fe9f937fc37ec29bd590f0b5a7f92974204f3551
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-08-13 13:30:42 +0700

    add bcrypt

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var UserRouter = express.Router();
+const bcrypt = require('bcrypt');
 const modelUser = require('../models/Users');
 const JWT = require('jsonwebtoken');
 const SECRET_KEY = "Namnv"
@@ -7,19 +8,32 @@ const SECRET_KEY = "Namnv"
 // Login user
 UserRouter.post('/login', async (req, res) => {
     try {
-        if (!username || !email || !password) {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
             return res.status(400).json({
-                message: 'Username, email, and password are required.'
+                message: 'Username and password are required.'
             });
         }
-        const { username, password } = req.body;
-        const user = await modelUser.findOne({ username, password });
-        if (user) {
+
+        // Tìm người dùng bằng username
+        const user = await modelUser.findOne({ username });
+
+        if (user && await bcrypt.compare(password, user.password)) {
+            // Tạo JWT token
             const token = JWT.sign({ id: user._id }, SECRET_KEY, { expiresIn: '1h' });
             const refreshToken = JWT.sign({ id: user._id }, SECRET_KEY, { expiresIn: '1d' });
+
+            // Gửi thông tin người dùng và token lên frontend
             res.json({
                 status: 200,
                 message: 'Đăng nhập thành công',
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    username: user.username,
+                    email: user.email,
+                },
                 token: token,
                 refreshToken: refreshToken
             });
