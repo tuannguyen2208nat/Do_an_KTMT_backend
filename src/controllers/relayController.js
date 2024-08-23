577c6ace0726c9a6b7fc95743ed584db5507909c
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-08-23 14:14:41 +0700

    edit relay function

--- src/controllers/relayController.js
+++ src/controllers/relayController.js
@@ -73,37 +73,21 @@ const set_relay = async (req, res, next) => {
 const set_status = async (req, res, next) => {
     try {
         const userID = req.user.id;
-        const { relay_id } = req.body;
+        const { relay_id, state } = req.body;
         if (!relay_id) {
             return res.status(400).json({ error: 'Relay id is required.' });
+        }
+        if (state === undefined) {
+            return res.status(400).json({ error: 'State is required.' });
         }
         const relay = await Relay.findOne({ relay_id: relay_id, userID: userID });
         if (!relay) {
             return res.status(404).json({ error: 'Relay not found.' });
         }
-        relay.state = !relay.state;
+        relay.state = state;
         await relay.save();
         req.activity = `Relay ${relay_id} state changed to ${relay.state ? 'on' : 'off'}`;
         next();
-    }
-    catch (error) {
-        console.error('Error setting data:', error);
-        res.status(500).json({ error: 'An error occurred while setting data.' });
-    }
-}
-
-const get_status = async (req, res) => {
-    try {
-        const userID = req.user.id;
-        const { relay_id } = req.body;
-        if (!relay_id) {
-            return res.status(400).json({ error: 'Relay id is required.' });
-        }
-        const relay = await Relay.findOne({ relay_id: relay_id, userID: userID });
-        if (!relay) {
-            return res.status(404).json({ error: 'Relay not found.' });
-        }
-        res.status(200).json({ state: relay.state });
     }
     catch (error) {
         console.error('Error setting data:', error);
@@ -136,7 +120,6 @@ module.exports = {
     add_relay,
     get_relay,
     set_relay,
-    get_status,
     set_status,
     delete_relay,
 };
--- src/routes/relayRoutes.js
+++ src/routes/relayRoutes.js
@@ -8,6 +8,5 @@ RelayRoutes.get('/get', relayController.get_relay);
 RelayRoutes.patch('/set', relayController.set_relay, logController.setLog);
 RelayRoutes.delete('/delete', relayController.delete_relay, logController.setLog);
 RelayRoutes.patch('/set-status', relayController.set_status, logController.setLog);
-RelayRoutes.get('/get-status', relayController.get_status);
 
 module.exports = RelayRoutes;
