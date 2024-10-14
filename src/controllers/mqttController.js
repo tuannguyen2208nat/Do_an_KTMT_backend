088ff07c1bf78f2ea94f5f6b6fb9c07b27374eac
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-10-15 00:50:51 +0700

    add feed History in MQTT

--- src/controllers/mqttController.js
+++ src/controllers/mqttController.js
@@ -11,6 +11,39 @@ let AIO_USERNAME;
 
 let client = null;
 
+const saveData = async (type, data, time) => {
+    if (type === 'temp') {
+        const tempData = new TemperatureSensor({
+            userID: userId,
+            data: data,
+            Date: time,
+        });
+        await tempData.save();
+        console.log('Temperature data saved to MongoDB');
+    }
+    else if (type === 'humi') {
+        const humidityData = new HumiditySensor({
+            userID: userId,
+            data: data,
+            Date: time,
+        });
+        await humidityData.save();
+        console.log('Humidity data saved to MongoDB');
+    }
+    else if (type === 'location') {
+        const [X, Y] = data.split('-');
+        const locationData = new Location({
+            userID: userId,
+            X: X,
+            Y: Y,
+            Date: time,
+        });
+        await locationData.save();
+        console.log('Location data saved to MongoDB');
+    }
+
+};
+
 const connect = async (req, res) => {
     const userId = req.user.id;
     const user = await modelUser.findById(userId).exec();
@@ -51,8 +84,9 @@ const subscribeToFeeds = (client, AIO_USERNAME, userId) => {
     const tempFeed = `${AIO_USERNAME}/feeds/temperature`;
     const humFeed = `${AIO_USERNAME}/feeds/humidity`;
     const locationFeed = `${AIO_USERNAME}/feeds/location`;
+    const historyFeed = `${AIO_USERNAME}/feeds/history`;
 
-    [tempFeed, humFeed, locationFeed].forEach((feed) => {
+    [tempFeed, humFeed, locationFeed, historyFeed].forEach((feed) => {
         client.subscribe(feed, (err) => {
             if (err) {
                 console.error('Subscription error:', err);
@@ -68,33 +102,33 @@ const subscribeToFeeds = (client, AIO_USERNAME, userId) => {
         try {
             if (feed.includes('temperature')) {
                 console.log(`Temperature data received: ${data}°C`);
-                const tempData = new TemperatureSensor({
-                    userID: userId,
-                    data: parseFloat(data),
-                    Date: new Date(),
-                });
-                await tempData.save();
-                console.log('Temperature data saved to MongoDB');
+                saveData('temp', parseFloat(data), new Date());
             } else if (feed.includes('humidity')) {
                 console.log(`Humidity data received: ${data}%`);
-                const humidityData = new HumiditySensor({
-                    userID: userId,
-                    data: parseFloat(data),
-                    Date: new Date(),
-                });
-                await humidityData.save();
-                console.log('Humidity data saved to MongoDB');
+                saveData('humi', parseFloat(data), new Date());
             } else if (feed.includes('location')) {
                 console.log(`Location data received: ${data}`);
-                const [X, Y] = data.split('-');
-                const locationData = new Location({
-                    userID: userId,
-                    X: X,
-                    Y: Y,
-                    Date: new Date(),
-                });
-                await locationData.save();
-                console.log('Location data saved to MongoDB');
+                saveData('location', data, new Date());
+            }
+            else if (feed.includes('history')) {
+                console.log(`History data received: ${data}`);
+                const historyData = JSON.parse(data);
+                const { mode, time } = historyData;
+
+                const [hourMinute, dayMonthYear] = time.split('-');
+                const [hour, minute] = hourMinute.split(':');
+                const [day, month, year] = dayMonthYear.split(':');
+                const timeDate = new Date(year, month - 1, day, hour, minute);
+
+                if (mode === 'temp_humi') {
+                    const { temperature, humidity } = historyData;
+                    saveData('temp', parseFloat(temperature), timeDate);
+                    saveData('humi', parseFloat(humidity), timeDate);
+                }
+                else if (mode === 'location') {
+                    const { data } = historyData;
+                    saveData('location', data, timeDate);
+                }
             }
         } catch (error) {
             console.error('Error saving data to MongoDB:', error);
