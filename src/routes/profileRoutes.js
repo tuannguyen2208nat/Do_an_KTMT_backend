dcb3bcddf5c09427d45fac10d74493d2e1c04f06
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-12-26 19:20:36 +0700

    add Ipmiddleware for profile

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -3,9 +3,10 @@ var ProfileRouter = express.Router();
 const Usercontroller = require('../controllers/userController');
 const upload = require('../middlewares/uploadMiddleware');
 const mqttController = require('../connect/mqttController')
+const IpMiddleware = require('../middlewares/IpMiddleware');
 
-ProfileRouter.patch('/edit', upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'coverPhoto', maxCount: 1 }]), Usercontroller.edit_profile, mqttController.reconnectMqtt);
-ProfileRouter.delete('/delete', mqttController.disconnectMqtt, Usercontroller.delete_profile)
-ProfileRouter.get('/', Usercontroller.get_profile);
+ProfileRouter.patch('/edit', IpMiddleware('edit profile'), upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'coverPhoto', maxCount: 1 }]), Usercontroller.edit_profile, mqttController.reconnectMqtt);
+ProfileRouter.delete('/delete', IpMiddleware('delete profile'), mqttController.disconnectMqtt, Usercontroller.delete_profile)
+ProfileRouter.get('/', IpMiddleware('get profile'), Usercontroller.get_profile);
 
 module.exports = ProfileRouter;
