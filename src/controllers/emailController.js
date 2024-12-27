58b264b7e0435524055ced31bb3309a791274311
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-12-28 01:31:24 +0700

    delete confirm code

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const crypto = require('crypto');
 const Transporter = require('../config/email');
 
-const verificationCodes = {};
-
 const generateVerificationCode = () => {
     return crypto.randomBytes(3).toString('hex');
 };
@@ -28,12 +26,11 @@ const send_code = async (req, res) => {
             });
         }
         const code = generateVerificationCode();
-        verificationCodes[email] = code;
         const subject = 'Verification code';
         const text = `Your verification code is ${code}`;
         await sendVerificationCode(email, subject, text);
         return res.status(200).json({
-            message: 'Verification code sent successfully',
+            code,
         });
     }
     catch (error) {
@@ -43,36 +40,36 @@ const send_code = async (req, res) => {
     }
 }
 
-const confirm_code = async (req, res) => {
-    try {
-        const { email } = req;
-        const { verificationCode } = req.body;
+// const confirm_code = async (req, res) => {
+//     try {
+//         const { email } = req;
+//         const { verificationCode } = req.body;
 
-        if (!email || !verificationCode) {
-            return res.status(400).json({
-                error: 'Email and code are required.',
-            });
-        }
-        const storedCode = verificationCodes[email];
-        if (storedCode === verificationCode) {
-            delete verificationCodes[email];
-            return res.status(200).json({
-                message: 'Verification successful.',
-            });
-        } else {
-            return res.status(400).json({
-                error: 'Invalid verification code.',
-            });
-        }
-    }
-    catch (error) {
-        return res.status(500).json({
-            error: 'Server error',
-        });
-    }
-}
+//         if (!email || !verificationCode) {
+//             return res.status(400).json({
+//                 error: 'Email and code are required.',
+//             });
+//         }
+//         const storedCode = verificationCodes[email];
+//         if (storedCode === verificationCode) {
+//             delete verificationCodes[email];
+//             return res.status(200).json({
+//                 message: 'Verification successful.',
+//             });
+//         } else {
+//             return res.status(400).json({
+//                 error: 'Invalid verification code.',
+//             });
+//         }
+//     }
+//     catch (error) {
+//         return res.status(500).json({
+//             error: 'Server error',
+//         });
+//     }
+// }
 
 module.exports = {
     send_code,
-    confirm_code,
+    // confirm_code,
 };
--- src/routes/emailRoutes.js
+++ src/routes/emailRoutes.js
@@ -4,6 +4,6 @@ const emailController = require('../controllers/emailController');
 const emailorusername = require('../middlewares/emailorusername');
 
 EmailRoutes.post('/send-code', emailorusername, emailController.send_code);
-EmailRoutes.post('/confirm-code', emailorusername, emailController.confirm_code);
+// EmailRoutes.post('/confirm-code', emailorusername, emailController.confirm_code);
 
 module.exports = EmailRoutes;
